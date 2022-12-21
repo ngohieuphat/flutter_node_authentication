64ff0f5f97813154538a09c52b3b9ee491d23644
ngohieuphat <ngohipha1@gmail.com>
2022-12-21 16:21:48 +0700

    Update landing.dart
    logout

--- lib/src/landing.dart
+++ lib/src/landing.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nodejs/src/loginsection.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LandingScreen extends StatelessWidget {
   static  String id = "landingscreen";
@@ -6,6 +8,23 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("abc");
+
+     return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(child: Text("Welcome to the Landing Screen")),
+        TextButton.icon(
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              // null co khong co ma thong qa k tu dong dang nhap 
+              await prefs.setString('token','null' );
+              Navigator.pushNamed(context, LoginSection.id);
+            },
+            icon: Icon(Icons.send),
+            label: Text("Logout"))
+      ],
+    ));
+  
   }
 }
